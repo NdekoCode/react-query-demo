a636e56e075cea538eb00aa38fcf334bf42ff956
NdekoCode <arickbulakali@gmail.com>
2025-05-31 06:57:58 +0200

    chore: update to tailwindcss v4

--- src/components/UserLine.tsx
+++ src/components/UserLine.tsx
@@ -9,7 +9,7 @@ export const UserLine = ({ user }: UserLineProps) => {
   return (
     <Link
       href={`/users/${user.id}`}
-      className="block w-fit p-6  rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700"
+      className="block w-fit p-6  rounded-lg shadow-sm bg-gray-800 border-gray-700 hover:bg-gray-700"
     >
       <h5 className="text-2xl font-bold tracking-tight text-white">{user.name}</h5>
       <p className="text-gray-400">{user.email}</p>
